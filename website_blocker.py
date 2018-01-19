db99ecd00453fc52263288b62310b415cdf00a55
Nnamdi Kalu <nnamdi.kalu@gmail.com>
2018-01-19 13:03:56 -0500

    Made WebsiteBlocker class and fixed bug where sites were not deleted from host files

--- website_blocker.py
+++ website_blocker.py
@@ -3,83 +3,79 @@ from datetime import timedelta as td
 import time
 import os
 
-host_paths = ["C:\Windows\System32\drivers\etc\hosts","/etc/hosts"]
-redirect_path = "127.0.0.1"
-
-def website_list():
-    list_of_sites = []
-    sites = input("Please input the site you would like to block separated by commas: \n")
-
-    initial_list = sites.split(",")
-
-    for x in initial_list:
-        if x.startswith("www"):
-            list_of_sites.append(x)
-            list_of_sites.append(x.strip("www."))
+class WebsiteBlocker():
+    def __init__(self):
+        if os.name == 'nt':
+            self.host_path = "C:\Windows\System32\drivers\etc\hosts"
         else:
-            list_of_sites.append(x)
-            list_of_sites.append("www.%s" % x)
-
-    return list_of_sites
-
-
-def time_to_be_blocked():
-    time_units = ["hours", "minutes", "seconds"]
-    time_blocked = input("How long would you like to block these websites in "
-                         "hours, minutes and seconds(ex. 2 hours 3 minutes and 30 seconds)?: \n")
+            self.host_path = "/etc/hosts"
+        self.redirect_path = "127.0.0.1"
+        self.sites_to_be_blocked = []
 
 
-    if  any(x in time_blocked.lower() for x in time_units):
-        time_blocked = [int(x) for x in time_blocked.split() if x.isdigit()]
-    else:
-        print("Please include hours, minutes and seconds in your answer")
-        time_to_be_blocked()
 
-    return time_blocked
+    def website_list(self):
+        sites = input("Please input the site you would like to block separated by commas: \n")
+
+        initial_list = sites.split(",")
+
+        for x in initial_list:
+            if x.startswith("www"):
+                self.sites_to_be_blocked.append(x)
+                self.sites_to_be_blocked.append(x.strip("www."))
+            else:
+                self.sites_to_be_blocked.append(x)
+                self.sites_to_be_blocked.append("www.%s" % x)
 
 
-def add_to_host(file_content, list_of_websites, host_file):
-    for website in list_of_websites:
-        if website in file_content:
-            pass
+    def time_to_be_blocked(self):
+        time_units = ["hours", "minutes", "seconds"]
+        time_blocked = input("How long would you like to block these websites in "
+                             "hours, minutes and seconds(ex. 2 hours 3 minutes and 30 seconds)?: \n")
+
+
+        if  any(x in time_blocked.lower() for x in time_units):
+            time_blocked = [int(x) for x in time_blocked.split() if x.isdigit()]
         else:
-            host_file.write(redirect_path + ' ' + website + '\n')
+            print("Please include hours, minutes and seconds in your answer")
+            self.time_to_be_blocked()
+
+        return time_blocked
 
 
-def del_from_host(file_content, list_of_websites):
-    file_content.seek(0)
-    for line in file_content:
-        if not any(website in line for website in list_of_websites):
-            file_content.write(line)
-    file_content.truncate()
+    def add_to_host(self, file_content, list_of_websites, host_file):
+        for website in list_of_websites:
+            if website in file_content:
+                pass
+            else:
+                host_file.write(self.redirect_path + ' ' + website + '\n')
 
 
-list_of_websites = website_list()
+    def del_from_host(self, file_content, list_of_websites):
+        content = file_content.readlines()
+        file_content.seek(0)
+        for line in content:
+            if not any(website in line for website in list_of_websites):
+                file_content.write(line)
+        file_content.truncate()
 
-time_blocked = time_to_be_blocked()
+wb = WebsiteBlocker()
+wb.website_list()
+list_of_websites = wb.sites_to_be_blocked
+time_blocked = wb.time_to_be_blocked()
+
 
 start_time = dt.now()
 end_time = start_time + td(hours=time_blocked[0], minutes=time_blocked[1], seconds=time_blocked[2])
 
 while start_time < end_time:
-    time.sleep(5)
     print("Blocking sites...")
-    if os.name == 'nt':
-        with open(host_paths[0], 'r+') as host:
-            content = host.read()
-            add_to_host(content, list_of_websites, host)
-    else:
-        with open(host_paths[1], 'r+') as host:
-            content = host.read()
-            add_to_host(content, list_of_websites, host)
+
+    with open(wb.host_path, 'r+') as host:
+        content = host.read()
+        wb.add_to_host(content, list_of_websites, host)
     start_time = dt.now()
+    time.sleep(5)
 
-
-if os.name == 'nt':
-    with open(host_paths[0], 'r+') as host:
-        content = host.readlines()
-        del_from_host(host, list_of_websites)
-else:
-    with open(host_paths[1], 'r+') as host:
-        content = host.readlines()
-        del_from_host(host, list_of_websites)
+with open(wb.host_path, 'r+') as host:
+    wb.del_from_host(host, list_of_websites)
